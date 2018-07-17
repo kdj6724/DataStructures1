db9487a2140b73771c53e7c85556ab2ab271a9c4
kdj6724 <kdj6724@gmail.com>
2018-07-17 19:03:01 +0900

    2장 하다 말았음

---
+++ ch2/Polynomial.h
@@ -0,0 +1,31 @@
+// Polynomial.h
+// 2018-07-17
+// kdj6724@gmail.com
+#include <cstdio>
+#ifndef CH2_POLYNOMIAL_H_
+#define CH2_POLYNOMIAL_H_
+#define MAX_DEGREE 80
+class Polynomial {
+private:
+  int degree_;
+  float coef_[MAX_DEGREE];
+public:
+  Polynomial() { 
+    degree_ = 0;
+  }
+  void Read() {
+    printf("다항식의 최고 차수를 입력하시오: ");
+    scanf("%d", degree_);
+    printf("각 항의 계수를 입력 하시오 (총 %d개): ", degree_ + 1);
+    for (int i=0; i<=degree_; i++)
+      scanf("%f", coef + i);
+  }
+  void Display(char* str=" Poly = ") {
+    printf("\t %s", str);
+    for(int i=0; i<degree_; i++)
+      printf("%5.1f x^%d + ", coef_[i], degree_ - i);
+    printf("%4.1f\n", cofe_[degree_]);
+  }
+  void Add(
+};
+#endif  // CH2_POLYNOMIAL_H_
