4a2e826f98582be2703c70015d45805ad6514e96
kdj6724 <kdj6724@gmail.com>
2018-07-17 17:38:02 +0900

    테스트 결과 출력 하도록 수정

--- ch1/example.cpp
+++ ch1/example.cpp
@@ -2,14 +2,18 @@
 // 2018-07-17
 // kdj6724@gmail.com
 #include <iostream>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
 
+// O(1)
 int sumAlgorithmA(int n) {
   int res = 0;
   res = n*(n + 1)/2;
   return res;
 }
 
+// O(n)
 int sumAlgorithmB(int n) {
   int res = 0;
   for(int i=1; i<=n; i++) {
@@ -18,6 +22,7 @@ int sumAlgorithmB(int n) {
   return res;
 }
 
+// O(n^2)
 int sumAlgorithmC(int n) {
   int res = 0;
   for(int i=0; i<=n; i++) {
@@ -29,14 +34,30 @@ int sumAlgorithmC(int n) {
 }
 
 int main(int argc, char* argv[]) {
+  clock_t start, finish;
+  double duration;
   int val = 0;
   if (argc < 2) {
     printf("argument error\n");
     return -1;
   }
   val = atoi(argv[1]);
-  std::cout << "sumAlgorithmA " <<  sumAlgorithmA(val) << std::endl;
-  std::cout << "sumAlgorithmB " <<  sumAlgorithmB(val) << std::endl;
-  std::cout << "sumAlgorithmC " <<  sumAlgorithmC(val) << std::endl;
+  start = clock();
+  std::cout << "sumAlgorithmA " <<  sumAlgorithmA(val);
+  finish = clock();
+  duration = (double)finish - start;
+  printf(" -> runtime : %f\n", duration);
+  
+  start = clock();
+  std::cout << "sumAlgorithmB " <<  sumAlgorithmB(val);
+  finish = clock();
+  duration = (double)finish - start;
+  printf(" -> runtime : %f\n", duration);
+
+  start = clock();
+  std::cout << "sumAlgorithmC " <<  sumAlgorithmC(val);
+  finish = clock();
+  duration = (double)finish - start;
+  printf(" -> runtime : %f\n", duration);
   return 0;
 }
